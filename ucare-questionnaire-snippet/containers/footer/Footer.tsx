a6bc32b674955d14efb712143d188340fd194e89
Alma <Alotte@users.noreply.github.com>
2023-06-02 18:50:16 +0200

    Hotfix Sticky Footer

--- ucare-questionnaire-snippet/containers/footer/Footer.tsx
+++ ucare-questionnaire-snippet/containers/footer/Footer.tsx
@@ -6,35 +6,35 @@ import uppsala_uni from '../../public/assets/footer/logo-uppsala-uni.png';
 
 const Footer = () => {
   return (
-    <div className={styles.footer}> 
-    <div className={styles.footer_infos}>
-      <Image
-            src= {logomark}
-            alt="U-CARE Logo"
-            width={120}
-      />
-      <div className={styles.footer_infos__text}>
-        <p>
-          U-CARE
-        </p>
-        <p>  
-          Psychosocial care in the interactive society
-        </p>
-        <p>
-          Last update 2022-05-02
-        </p>
+    <footer className={styles.footer}>
+      <div className={styles.footer_infos}>
+        <Image
+              src= {logomark}
+              alt="U-CARE Logo"
+              width={120}
+        />
+        <div className={styles.footer_infos__text}>
+          <p>
+            U-CARE
+          </p>
+          <p>  
+            Psychosocial care in the interactive society
+          </p>
+          <p>
+            Last update 2022-05-02
+          </p>
 
+        </div>
+        <Image
+              src= {uppsala_uni}
+              alt="U-CARE Logo"
+        />
       </div>
-      <Image
-            src= {uppsala_uni}
-            alt="U-CARE Logo"
-      />
-    </div>
-    <div className={styles.footer_support}>
-      <p>Technical problems?</p>
-      <a href='#support-contact'>Contact Support.</a>
-    </div>
-    </div>
+      <div className={styles.footer_support}>
+        <p>Technical problems?</p>
+        <a href='#support-contact'>Contact Support.</a>
+      </div>
+    </footer>
   )
 }
 
