2e6196e9d3c2d4add3a7f1d50a1d39706dd2d826
Alma <Alotte@users.noreply.github.com>
2023-06-05 21:15:54 +0200

    QuestionnaireNav is finished
    Added some global colors.

--- ucare-questionnaire-snippet/components/questionnaire-nav/QuestionnaireNav.tsx
+++ ucare-questionnaire-snippet/components/questionnaire-nav/QuestionnaireNav.tsx
@@ -3,9 +3,34 @@ import styles from './QuestionnaireNav.module.css'
 
 type Props = {}
 
-const QuestionnaireNav = (props: Props) => {
+const NavItem = ({ title, inFocus }: { title: string; inFocus: boolean }) => {
+    if (inFocus) {
+      return (
+        <div className= {styles.questionnaireNav_itemContainer}>
+          <div className={styles.questionnaireNav_itemContainer_active}>
+            <h4  className={styles.questionnaireNav_item__active}>{title}</h4>
+            <div className= {styles.questionnaireNav_horizontalLine__active}></div>
+          </div>
+        </div>
+    )
+    } else {
+      return (
+        <div className= {styles.questionnaireNav_itemContainer}>
+            <h4 className={styles.questionnaireNav_item__inactive}>{title}</h4>
+            <div className= {styles.questionnaireNav_horizontalLine__inactive}></div>
+        </div>
+      )
+    }
+}
+
+const QuestionnaireNav = (props) => {
   return (
-    <div>QuestionnaireNav</div>
+    <div className={styles.questionnaireNav}>
+      <NavItem title="Questions" inFocus= {true} />
+      <NavItem title="Results" inFocus= {false} />
+      <NavItem title="Thank You Page" inFocus= {false} />
+      <div className={styles.questionnaireNav_horizontalLine__inactive}></div>
+    </div>
   )
 }
 
