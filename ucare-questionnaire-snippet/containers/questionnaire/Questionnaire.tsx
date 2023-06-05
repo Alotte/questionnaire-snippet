fa6038add82f1e2eeced8d3be8c9113ae99b20fd
Alma <Alotte@users.noreply.github.com>
2023-06-05 23:22:04 +0200

    Questionnaire (Title only)
    With Material Design style Input field

--- ucare-questionnaire-snippet/containers/questionnaire/Questionnaire.tsx
+++ ucare-questionnaire-snippet/containers/questionnaire/Questionnaire.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Questionnaire.module.css'
 
 type Props = {}
 
 const Questionnaire = ({ title, onTitleChange }) => {
+  const [value, setValue] = useState('');
+  const [showPlaceholder, setShowPlaceholder] = useState(true);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    setShowPlaceholder(e.target.value === '');
+  };
+
   return (
-    <div>
-      <label>Title:</label>
-      <input type="text" value={title} onChange={onTitleChange} />
+    <div className={styles.questionnaireTitle}>
+      <label className={styles.input__textfield_filled}>
+      <input 
+       type="text" 
+       value={title} 
+       onChange={onTitleChange}
+       placeholder="Questionnaire Title"
+       className={styles.questionnaireTitle_form__control}/>
+      <span>Questionnaire Title</span>
+    </label>
     </div>
   );
 };
