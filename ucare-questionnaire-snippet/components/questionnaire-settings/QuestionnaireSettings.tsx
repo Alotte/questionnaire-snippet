371f4dc0a8b47f6df0ccba19f8f39826c8acbdd7
Alma <Alotte@users.noreply.github.com>
2023-06-06 12:47:10 +0200

    QuestionnaireSettings

--- ucare-questionnaire-snippet/components/questionnaire-settings/QuestionnaireSettings.tsx
+++ ucare-questionnaire-snippet/components/questionnaire-settings/QuestionnaireSettings.tsx
@@ -1,11 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
+import { icArrowDown, icArrowDownBlue } from '../../public/assets'
 import styles from './QuestionnaireSettings.module.css'
 
 type Props = {}
 
 const QuestionnaireSettings = (props: Props) => {
   return (
-    <div>QuestionnaireSettings</div>
+    <div className={styles.qSettings}>
+      <label content='text'>Questionnaire Settings</label>
+      <div className={styles.qSettings_icon}>
+        <Image src={icArrowDownBlue} alt="accordion" />
+      </div>
+    </div>
   )
 }
 
