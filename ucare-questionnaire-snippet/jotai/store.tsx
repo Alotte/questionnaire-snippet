02247af6cb6beacb59e312e16dfdf530bcfa665c
Alma <Alotte@users.noreply.github.com>
2023-06-06 21:39:15 +0200

    Fixed nested project structure

--- ucare-questionnaire-snippet/jotai/store.tsx
+++
@@ -1,8 +0,0 @@
-import { atomWithStorage } from "jotai/utils";
-
-export interface Cookie {
-    access: string;
-    refresh: string;
-}
-const questionnaireTitleAtom = atomWithStorage('title', "");
-
